5469a67cc2c9fd313882e3e94701aaabf78d0d55
Tiago Dornelas <>
2024-12-12 15:34:28 -0300

    init upgrade

--- modules-ps-game/init.js
+++ modules-ps-game/init.js
@@ -1,184 +1,55 @@
-import divGame from "./divGame.js";
-import { gameSection } from "./game-setup.js";
-import selectQT from "./selectQT.js";
-import inputQT from "./inputQT.js";
-import { rdPt } from "./game-setup.js";
+import divGame from "./divGame.js"; 
+import { gameSection, rdPt } from "./game-setup.js"; 
+import selectQT from "./selectQT.js"; 
+import inputQT from "./inputQT.js"; 
 import { createBtn } from "./create-btn.js";
 
-export default function init() {
+export default async function teste(){
   divGame.appendChild(gameSection);
   while (gameSection.firstChild) {
     gameSection.removeChild(gameSection.firstChild);
-  }
+  };
+  console.log("testetstetsetstes")
+  const loadQuestion = async (round) =>{
+    const module = await import("./questions.js");
+    const question = module[`qt${round}`];
+    const btn = createBtn(`${round}st question`);
 
-  if (rdPt.round === 1) {
-    let qt1;
-    import("./questions.js").then((module) => {
-      qt1 = module.qt1;
-    });
+    if(question.type === "input"){
+      gameSection.appendChild(btn);
+      btn.addEventListener("click", (e) => {
+        e.stopPropagation();
+        inputQT(question);
+      });   
+    }else{
+        gameSection.appendChild(btn);
+        btn.addEventListener("click", (e) => {
+          e.stopPropagation();
+          selectQT(question);
+        });
+    };
+  };
 
+  const createH3 = (text) => {
     const h3 = document.createElement("h3");
-    h3.textContent = "Quiz: PlayStation Knowledge";
+    h3.textContent = text;
     h3.style.fontFamily = "Courier New";
     h3.style.borderBottom = "2px solid black";
     h3.style.padding = "0";
     h3.style.margin = "0";
     gameSection.appendChild(h3);
-    
-    const btn1 = createBtn("1st question");
-    gameSection.appendChild(btn1);
-    btn1.addEventListener("click", (e) => {
-      e.stopPropagation();
+  };
 
-      inputQT(qt1);
-    });
-  }
-
-  if (rdPt.round === 2) {
-    let qt2;
-    import("./questions.js").then((module) => {
-      qt2 = module.qt2;
-    });
-
-    const btn2 = createBtn("2st question");
-    gameSection.appendChild(btn2);
-
-    btn2.addEventListener("click", (e) => {
-      e.stopPropagation();
-
-      selectQT(qt2);
-    });
-  }
-
-  if (rdPt.round === 3) {
-    let qt3;
-    import("./questions.js").then((module) => {
-      qt3 = module.qt3;
-    });
-
-    const btn3 = createBtn("3st question");
-    gameSection.appendChild(btn3);
-
-    btn3.addEventListener("click", (e) => {
-      e.stopPropagation();
-      selectQT(qt3);
-    });
-  }
-
-  if (rdPt.round === 4) {
-    let qt4;
-    import("./questions.js").then((module) => {
-      qt4 = module.qt4;
-    });
-
-    const btn4 = createBtn("4st question");
-    gameSection.appendChild(btn4);
-
-    btn4.addEventListener("click", (e) => {
-      e.stopPropagation();
-      selectQT(qt4);
-    });
-  }
-
-  if (rdPt.round === 5) {
-    let qt5;
-    import("./questions.js").then((module) => {
-      qt5 = module.qt5;
-    });
-
-    const btn5 = createBtn("5st question");
-    gameSection.appendChild(btn5);
-
-    btn5.addEventListener("click", (e) => {
-      e.stopPropagation();
-      inputQT(qt5);
-    });
-  }
-
-  if (rdPt.round === 6) {
-    let qt6;
-    import("./questions.js").then((module) => {
-      qt6 = module.qt6;
-    });
-
-    const btn6 = createBtn("6st question");
-    gameSection.appendChild(btn6);
-
-    btn6.addEventListener("click", (e) => {
-      e.stopPropagation();
-      inputQT(qt6);
-    });
-  }
-
-  if (rdPt.round === 7) {
-    let qt7;
-    import("./questions.js").then((module) => {
-      qt7 = module.qt7;
-    });
-
-    const btn7 = createBtn("7st question");
-    gameSection.appendChild(btn7);
-
-    btn7.addEventListener("click", (e) => {
-      e.stopPropagation();
-      selectQT(qt7);
-    });
-  }
-
-  if (rdPt.round === 8) {
-    let qt8;
-    import("./questions.js").then((module) => {
-      qt8 = module.qt8;
-    });
-
-    const btn8 = createBtn("8st question");
-    gameSection.appendChild(btn8);
-
-    btn8.addEventListener("click", (e) => {
-      e.stopPropagation();
-      inputQT(qt8);
-    });
-  }
-
-  if (rdPt.round === 9) {
-    let qt9;
-    import("./questions.js").then((module) => {
-      qt9 = module.qt9;
-    });
-
-    const btn9 = createBtn("9st question");
-    gameSection.appendChild(btn9);
-
-    btn9.addEventListener("click", (e) => {
-      e.stopPropagation();
-      inputQT(qt9);
-    });
-  }
-
-  if (rdPt.round === 10) {
-    let qt10;
-    import("./questions.js").then((module) => {
-      qt10 = module.qt10;
-    });
-
-    const btn10 = createBtn("10st question");
-    gameSection.appendChild(btn10);
-
-    btn10.addEventListener("click", (e) => {
-      e.stopPropagation();
-      inputQT(qt10);
-    });
-  }
-
-  if (rdPt.round > 10) {
-    const h3 = document.createElement("h3");
-    h3.textContent = "END";
-    h3.setAttribute("class", "endGameH3");
-    gameSection.appendChild(h3);
+  if (rdPt.round === 1){
+    createH3("Quiz: Playstation Knowledge");
+    await loadQuestion(1);
+  }else if(rdPt.round <= 10){
+    await loadQuestion(rdPt.round);
+  }else{
+    createH3("END");
 
     const p = document.createElement("p");
     p.textContent = `Pontuação: ${rdPt.points}/10`;
-    p.setAttribute("class", "endGameP");
     gameSection.appendChild(p);
 
     const creditsDiv = document.createElement("div");
--- modules-ps-game/questions.js
+++ modules-ps-game/questions.js
@@ -1,4 +1,6 @@
 const qt1 = {
+  type: "input",
+
   qt: "What is the best-selling Playstation franchise?",
 
   info: "Gran Turismo, made by Polyphony Digital, is the best-selling franchise on PlayStation, with more than 90 million copies sold across 8 main games (Gran Turismo, Gran Turismo 2, Gran Turismo 3: A-Spec, Gran Turismo 4, Gran Turismo 5, Gran Turismo 6, Gran Turismo Sport, and Gran Turismo 7), and 8 other titles, including prologues and concepts. Gran Turismo is an icon both inside and outside the gaming world, revered by drivers and automakers from all over the globe. This recognition throughout the automotive world brought the Vision program to the game, which consists of partnerships with automakers such as Lamborghini, Nissan, Mercedes-Benz, Bugatti, among others. In this partnership, concept cars are created with revised visuals between the manufacturers and Polyphony Digital, and these concept cars are included in the games for free. The franchise is currently on a roll, with the latest game, Gran Turismo 7, being the best-selling game in the franchise.",
@@ -13,6 +15,8 @@ const qt1 = {
 };
 
 const qt2 = {
+  type: "select",
+
   qt: "In God of War Ragnarok, for what reason Draupnir Spear was made?",
 
   info: null,
@@ -39,6 +43,8 @@ const qt2 = {
 };
 
 const qt3 = {
+  type: "select",
+
   qt: "Playstation already won 3 GOTY awards (best game of the year), whice games were the winners?",
 
   info: "Naughty Dog's Uncharted 2 was named Game of the Year in 2009. The game innovated and marked the industry with its action scenes in both cutscenes and gameplay. Santa Monica Studio's God of War was named Game of the Year in 2018. The game completely reinvented the franchise, bringing one of the best combat systems ever made. Its story and characters are remarkable and brought a new perspective to Kratos, one of the greatest characters in the gaming world, showing in more depth who this character really is. The Last of Us Part II, also from Naughty Dog, was named Game of the Year in 2020. The game brought what was expected from Naughty Dog: technically, the game borders on perfection, with unique details and animations, combat beyond any other TPS, and a risky and harrowing story like never before.",
@@ -66,6 +72,8 @@ const qt3 = {
 
 
 const qt4 = {
+  type: "select",
+
   qt: "In Ghost of Tsushima, what are the names of the four combat stances that Jin Sakay masters?",
 
   info: null,
@@ -93,6 +101,8 @@ const qt4 = {
 };
 
 const qt5 = {
+  type: "input",
+
   qt: "In Uncharted, what is the quote written on Nathan Drake's ring?",
 
   info: null,
@@ -107,6 +117,8 @@ const qt5 = {
 };
 
 const qt6 = {
+  type: "input",
+
   qt: "In Horizon Forbidden West (in the base game) what type of machine can Aloy fly in?",
 
   info: null,
@@ -121,6 +133,8 @@ const qt6 = {
 };
 
 const qt7 = {
+  type: "select",
+
   qt: "In Infamous Second Son, what are the four powers absorbed by Delsin along the journey?",
 
   info: "Delsin's natural power allows him to absorb any power from other conductors, making him one of the most powerful conductors. Along the journey in Infamous Second Son, Delsin absorbed four powers: smoke, neon, video, and concrete.",
@@ -147,6 +161,8 @@ const qt7 = {
 };
 
 const qt8 = {
+  type: "input",
+
   qt: "In God of War, what is the name of Kratos brothers, whos has born with red marks that Kratos cariies in his memory?",
 
   info: "Deimos is Kratos' younger brother. He was kidnapped in his childhood while training alongside Kratos. Ares was the one who kidnapped him on Zeus' orders. Zeus' intention was to prevent the fulfillment of a prophecy that predicted the destruction of Olympus at the hands of a marked warrior. The gods of Olympus believed Deimos was the warrior of the prophecy due to his birthmark (the red stripe). During the kidnapping, Kratos tried to prevent Ares from taking him, but Ares burned him and scarred his eye. Soon after, Kratos made a red tattoo on his body, identical to Deimos' birthmark, in memory of his brother.",
@@ -161,6 +177,8 @@ const qt8 = {
 };
 
 const qt9 = {
+  type: "input",
+
   qt: "In Shadow of the Colossus, what is the name of the mysterious Colossus that watches over the heavens and is never seen?",
 
   info: "Shadow of the Colossus",
@@ -175,6 +193,8 @@ const qt9 = {
 };
 
 const qt10 = {
+  type: "input",
+  
   qt: "By becoming the God of War, Kratos represented pain, suffering, and destruction. He chases a redemption that he knows he can never deserve. But he has something that he will always carry, and that is what he wants to represent. What does Kratos want to represent?",
 
   info: "What is said about Kratos' redemption is that he became a new man, but this is a very simplistic way of looking at Kratos' entire story. There was a Kratos before and after 'losing' his family. Before that, he was disciplined and would do anything for those he had to protect, which is exactly what he does when he chases redemption. There is no forgiveness for what he did, and as much as he is afraid of losing everything and everyone and becoming what he has become, he is not looking for something new; he is looking to be who he really is.",
